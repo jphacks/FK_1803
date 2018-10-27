7f205651eb1377a8c69f2992059a019ae667cf5f
soufuru <soufuru1@gmail.com>
2018-10-27 22:24:50 +0900

    反応がない場合のメッセージを追加

--- app.js
+++ app.js
@@ -6,11 +6,19 @@ const clovaSkillHandler = clova.Client
 
   //起動時に喋る
   .onLaunchRequest(responseHelper => {
+    // まずはこっちをしゃべる
     responseHelper.setSimpleSpeech({
       lang: 'ja',
       type: 'PlainText',
       value: '忘れ物を探します',
     });
+
+    // しばらく反応がなければこちらをしゃべる
+    responseHelper.setSimpleSpeech({
+      lang: 'ja',
+      type: 'PlainText',
+      value: 'ものを探しますか？登録しますか？'
+    }, true);
   })
 
   //ユーザーからの発話が来たら反応する箇所
@@ -20,34 +28,44 @@ const clovaSkillHandler = clova.Client
 
     console.log('Intent:' + intent);
 
-    if (intent === 'otoja') {
-      responseHelper.setSimpleSpeech({
-        lang: 'ja',
-        type: 'PlainText',
-        value: `登録しました。`
-      })
-    }
-    if (intent === 'answer') {
-      const slots = responseHelper.getSlots();
-      console.log(slots);
-      console.log(slots.object);
-      let speech = {
-        lang: 'ja',
-        type: 'PlainText',
-        value: `${slots.object}は棚の上にあります。`
-      }
-      if (slots.area === '') {
-        speech.value = `捜し物の場所は登録されていません。`
-      }
-      responseHelper.setSimpleSpeech(speech);
-      responseHelper.setSimpleSpeech(speech, true);
+    switch (intent) {
+      case 'submit':
+        responseHelper.setSimpleSpeech({
+          lang: 'ja',
+          type: 'PlainText',
+          value: `登録しました。`
+        })
+        break;
+      case 'answer':
+        const slots = responseHelper.getSlots();
+        console.log(slots);
+        console.log(slots.object);
+        let speech = {
+          lang: 'ja',
+          type: 'PlainText',
+          value: `${slots.object}は棚の上にあります。`
+        }
+        if (slots.area === '') {
+          speech.value = `捜し物の場所は登録されていません。`
+        }
+        // まずはこっち
+        responseHelper.setSimpleSpeech(speech);
+        // responseHelper.setSimpleSpeech(speech, true);
+        break;
+      
+      case 'otoja':
+        responseHelper.setSimpleSpeech({
+          lang: 'ja',
+          type: 'PlainText',
+          value: 'おとじゃです'
+        })
+        break;
     }
   })
 
   //終了時
   .onSessionEndedRequest(responseHelper => {
     const sessionId = responseHelper.getSessionId();
-    console.log("Ended");
   })
   .handle();
 
